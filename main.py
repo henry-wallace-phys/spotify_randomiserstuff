8108ee9b0f35f0506e0ffbb65975bec3e39a2d14
henry-israel <henryisrael08@gmail.com>
2022-10-22 14:56:31 +0100

    probably should push to main, adds ability to enter own username

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from henrydle import henrydle_gui
 #Useful script, mostly for testing, will run Henrydle!
 
 if __name__=="__main__":
-    henrydle = henrydle_gui("dramallamaduck2")
+    username=input("Please Enter Username : ")
+    henrydle = henrydle_gui(username)
     henrydle()
